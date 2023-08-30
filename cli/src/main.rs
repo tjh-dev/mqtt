0e61b7a78b47dfdbbb187c73a7d86a2ac2c51bcf
Thom <14987462+thomhayward@users.noreply.github.com>
2023-08-30 18:52:08 +0100

    re-organise some more

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -8,7 +8,7 @@ struct Arguments {}
 async fn main() -> mqtt::Result<()> {
 	tracing_subscriber::fmt::init();
 
-	let (client, handle) = mqtt::tokio::client(("mqtt.tjh.dev", 1883));
+	let (client, handle) = mqtt::async_client::client(("mqtt.tjh.dev", 1883));
 	let result = client
 		.subscribe(vec![(String::from("#"), QoS::AtMostOnce)])
 		.await;
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -6,8 +6,8 @@ mod packet;
 mod qos;
 pub use qos::QoS;
 
-#[cfg(feature = "tokio")]
-pub mod tokio;
+#[cfg(feature = "async-client")]
+pub mod async_client;
 
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T> = std::result::Result<T, Error>;
