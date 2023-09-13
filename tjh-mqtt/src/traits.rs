10c488bd1124c4e6099549149ec9327d35f01bf5
Thom <14987462+thomhayward@users.noreply.github.com>
2023-09-13 09:27:55 +0100

    add more conversion trait impls

--- tjh-mqtt/src/traits.rs
+++ tjh-mqtt/src/traits.rs
@@ -1,14 +1,17 @@
 use crate::{FilterBuf, FilterError, InvalidTopic, QoS, TopicBuf};
 
 pub trait IntoFilters {
+	/// Convert `Self` into a [`Vec`] of [`FilterBuf`].
 	fn into_filters(self) -> Result<Vec<FilterBuf>, FilterError>;
 }
 
 pub trait IntoFiltersWithQoS {
+	/// Convert `Self` into a [`Vec`] of ([`FilterBuf`], [`QoS`]).
 	fn into_filters_with_qos(self) -> Result<Vec<(FilterBuf, QoS)>, FilterError>;
 }
 
 pub trait IntoTopicBuf {
+	/// Convert `Self` in to [`TopicBuf`].
 	fn into_topic_buf(self) -> Result<TopicBuf, InvalidTopic>;
 }
 
@@ -93,6 +96,52 @@ impl IntoFiltersWithQoS for Vec<(String, QoS)> {
 	fn into_filters_with_qos(self) -> Result<Vec<(FilterBuf, QoS)>, FilterError> {
 		let mut filters = Vec::with_capacity(self.len());
 		for (filter, qos) in self.into_iter() {
+			let filter = FilterBuf::new(filter)?;
+			filters.push((filter, qos))
+		}
+		Ok(filters)
+	}
+}
+
+impl IntoFiltersWithQoS for &[(&str, QoS)] {
+	fn into_filters_with_qos(self) -> Result<Vec<(FilterBuf, QoS)>, FilterError> {
+		let mut filters = Vec::with_capacity(self.len());
+		for (filter, qos) in self.iter() {
+			let filter = FilterBuf::new(*filter)?;
+			filters.push((filter, *qos))
+		}
+		Ok(filters)
+	}
+}
+
+impl IntoFiltersWithQoS for &[(String, QoS)] {
+	fn into_filters_with_qos(self) -> Result<Vec<(FilterBuf, QoS)>, FilterError> {
+		let mut filters = Vec::with_capacity(self.len());
+		for (filter, qos) in self.iter() {
+			let filter = FilterBuf::new(filter)?;
+			filters.push((filter, *qos))
+		}
+		Ok(filters)
+	}
+}
+
+impl IntoFiltersWithQoS for (&[&str], QoS) {
+	fn into_filters_with_qos(self) -> Result<Vec<(FilterBuf, QoS)>, FilterError> {
+		let (raw_filters, qos) = self;
+		let mut filters = Vec::with_capacity(raw_filters.len());
+		for filter in raw_filters.iter() {
+			let filter = FilterBuf::new(*filter)?;
+			filters.push((filter, qos))
+		}
+		Ok(filters)
+	}
+}
+
+impl IntoFiltersWithQoS for (&[String], QoS) {
+	fn into_filters_with_qos(self) -> Result<Vec<(FilterBuf, QoS)>, FilterError> {
+		let (raw_filters, qos) = self;
+		let mut filters = Vec::with_capacity(raw_filters.len());
+		for filter in raw_filters.iter() {
 			let filter = FilterBuf::new(filter)?;
 			filters.push((filter, qos))
 		}
