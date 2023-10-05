9f452df8db4ea1a668262dacb5fa85de2eb477ea
Thom <14987462+thomhayward@users.noreply.github.com>
2023-10-05 11:12:55 +0100

    improve conversions

--- tjh-mqtt/src/clients/conv.rs
+++ tjh-mqtt/src/clients/conv.rs
@@ -1,4 +1,4 @@
-use crate::{FilterBuf, InvalidFilter, QoS};
+use crate::{Filter, FilterBuf, InvalidFilter, QoS};
 
 /// A collection of FilterBuf.
 pub struct Filters(pub(crate) Vec<FilterBuf>);
@@ -6,45 +6,26 @@ pub struct Filters(pub(crate) Vec<FilterBuf>);
 /// A collection of (FilterBuf, QoS).
 pub struct FiltersWithQoS(pub(crate) Vec<(FilterBuf, QoS)>);
 
-impl TryFrom<&[&str]> for Filters {
+impl<'a, T: AsRef<str>> TryFrom<&[T]> for Filters {
 	type Error = InvalidFilter;
-	fn try_from(value: &[&str]) -> Result<Self, Self::Error> {
+	fn try_from(value: &[T]) -> Result<Self, Self::Error> {
 		let mut filters = Vec::with_capacity(value.len());
 		for s in value.iter() {
-			filters.push(FilterBuf::new(*s)?);
+			filters.push(s.as_ref().try_into()?);
 		}
 		Ok(Self(filters))
 	}
 }
 
-impl TryFrom<&[String]> for Filters {
+impl<E, T: TryInto<FilterBuf, Error = E>> TryFrom<Vec<T>> for Filters
+where
+	InvalidFilter: From<E>,
+{
 	type Error = InvalidFilter;
-	fn try_from(value: &[String]) -> Result<Self, Self::Error> {
-		let mut filters = Vec::with_capacity(value.len());
-		for s in value.iter() {
-			filters.push(FilterBuf::new(s)?);
-		}
-		Ok(Self(filters))
-	}
-}
-
-impl TryFrom<Vec<&str>> for Filters {
-	type Error = InvalidFilter;
-	fn try_from(value: Vec<&str>) -> Result<Self, Self::Error> {
+	fn try_from(value: Vec<T>) -> Result<Self, Self::Error> {
 		let mut filters = Vec::with_capacity(value.len());
 		for s in value.into_iter() {
-			filters.push(FilterBuf::new(s)?);
-		}
-		Ok(Self(filters))
-	}
-}
-
-impl TryFrom<Vec<String>> for Filters {
-	type Error = InvalidFilter;
-	fn try_from(value: Vec<String>) -> Result<Self, Self::Error> {
-		let mut filters = Vec::with_capacity(value.len());
-		for s in value.into_iter() {
-			filters.push(FilterBuf::new(s)?);
+			filters.push(s.try_into()?);
 		}
 		Ok(Self(filters))
 	}
@@ -53,106 +34,92 @@ impl TryFrom<Vec<String>> for Filters {
 impl TryFrom<&str> for FiltersWithQoS {
 	type Error = InvalidFilter;
 	fn try_from(value: &str) -> Result<Self, Self::Error> {
-		let filter = FilterBuf::new(value)?;
-		Ok(Self(vec![(filter, QoS::default())]))
+		Self::try_from(value.to_owned())
 	}
 }
 
 impl TryFrom<String> for FiltersWithQoS {
 	type Error = InvalidFilter;
+	#[inline]
 	fn try_from(value: String) -> Result<Self, Self::Error> {
 		let filter = FilterBuf::new(value)?;
 		Ok(Self(vec![(filter, QoS::default())]))
 	}
 }
 
-impl TryFrom<&[&str]> for FiltersWithQoS {
+impl TryFrom<&Filter> for FiltersWithQoS {
 	type Error = InvalidFilter;
-	fn try_from(value: &[&str]) -> Result<Self, Self::Error> {
-		let mut filters = Vec::with_capacity(value.len());
-		for raw_filter in value.iter() {
-			let filter = FilterBuf::new(*raw_filter)?;
-			filters.push((filter, QoS::default()));
-		}
-		Ok(Self(filters))
+	#[inline]
+	fn try_from(value: &Filter) -> Result<Self, Self::Error> {
+		Self::try_from(value.to_owned())
 	}
 }
 
-impl<const N: usize> TryFrom<[&str; N]> for FiltersWithQoS {
+impl TryFrom<FilterBuf> for FiltersWithQoS {
 	type Error = InvalidFilter;
-	fn try_from(value: [&str; N]) -> Result<Self, Self::Error> {
+	#[inline]
+	fn try_from(value: FilterBuf) -> Result<Self, Self::Error> {
+		Ok(Self(vec![(value, QoS::default())]))
+	}
+}
+
+impl<'a, T: AsRef<str>, const N: usize> TryFrom<[T; N]> for FiltersWithQoS {
+	type Error = InvalidFilter;
+	fn try_from(value: [T; N]) -> Result<Self, Self::Error> {
 		let mut filters = Vec::with_capacity(N);
-		for raw_filter in value.into_iter() {
-			let filter = FilterBuf::new(raw_filter)?;
-			filters.push((filter, QoS::default()));
+		for s in value.iter() {
+			filters.push((s.as_ref().try_into()?, QoS::default()));
 		}
 		Ok(Self(filters))
 	}
 }
 
-impl TryFrom<(&str, QoS)> for FiltersWithQoS {
+impl<'a, T: AsRef<str>> TryFrom<&[T]> for FiltersWithQoS {
 	type Error = InvalidFilter;
-	fn try_from(value: (&str, QoS)) -> Result<Self, Self::Error> {
-		let (raw_filter, qos) = value;
-		let filter = FilterBuf::new(raw_filter)?;
-		Ok(Self(vec![(filter, qos)]))
-	}
-}
-
-impl TryFrom<(String, QoS)> for FiltersWithQoS {
-	type Error = InvalidFilter;
-	fn try_from(value: (String, QoS)) -> Result<Self, Self::Error> {
-		let (raw_filter, qos) = value;
-		let filter = FilterBuf::new(raw_filter)?;
-		Ok(Self(vec![(filter, qos)]))
-	}
-}
-
-impl TryFrom<Vec<(&str, QoS)>> for FiltersWithQoS {
-	type Error = InvalidFilter;
-	fn try_from(value: Vec<(&str, QoS)>) -> Result<Self, Self::Error> {
+	fn try_from(value: &[T]) -> Result<Self, Self::Error> {
 		let mut filters = Vec::with_capacity(value.len());
-		for (raw_filter, qos) in value.into_iter() {
-			let filter = FilterBuf::new(raw_filter)?;
-			filters.push((filter, qos));
+		for s in value.iter() {
+			filters.push((s.as_ref().try_into()?, QoS::default()));
 		}
 		Ok(Self(filters))
 	}
 }
 
-impl TryFrom<Vec<(String, QoS)>> for FiltersWithQoS {
+impl<E, T: TryInto<FilterBuf, Error = E>> TryFrom<(T, QoS)> for FiltersWithQoS
+where
+	InvalidFilter: From<E>,
+{
 	type Error = InvalidFilter;
-	fn try_from(value: Vec<(String, QoS)>) -> Result<Self, Self::Error> {
+	fn try_from(value: (T, QoS)) -> Result<Self, Self::Error> {
+		let (filter, qos) = value;
+		Ok(Self(vec![(filter.try_into()?, qos)]))
+	}
+}
+
+impl<E, T: TryInto<FilterBuf, Error = E>> TryFrom<Vec<(T, QoS)>> for FiltersWithQoS
+where
+	InvalidFilter: From<E>,
+{
+	type Error = InvalidFilter;
+	fn try_from(value: Vec<(T, QoS)>) -> Result<Self, Self::Error> {
 		let mut filters = Vec::with_capacity(value.len());
-		for (raw_filter, qos) in value.into_iter() {
-			let filter = FilterBuf::new(raw_filter)?;
-			filters.push((filter, qos));
+		for (filter, qos) in value.into_iter() {
+			filters.push((filter.try_into()?, qos));
 		}
 		Ok(Self(filters))
 	}
 }
 
-impl TryFrom<(Vec<&str>, QoS)> for FiltersWithQoS {
+impl<E, T: TryInto<FilterBuf, Error = E>> TryFrom<(Vec<T>, QoS)> for FiltersWithQoS
+where
+	InvalidFilter: From<E>,
+{
 	type Error = InvalidFilter;
-	fn try_from(value: (Vec<&str>, QoS)) -> Result<Self, Self::Error> {
+	fn try_from(value: (Vec<T>, QoS)) -> Result<Self, Self::Error> {
 		let (raw_filters, qos) = value;
 		let mut filters = Vec::with_capacity(raw_filters.len());
 		for filter in raw_filters.into_iter() {
-			let filter = FilterBuf::new(filter)?;
-			filters.push((filter, qos))
-		}
-		Ok(Self(filters))
-	}
-}
-
-impl TryFrom<(Vec<String>, QoS)> for FiltersWithQoS {
-	type Error = InvalidFilter;
-	fn try_from(value: (Vec<String>, QoS)) -> Result<Self, Self::Error> {
-		let (raw_filters, qos) = value;
-		let mut filters = Vec::with_capacity(raw_filters.len());
-		for filter in raw_filters.into_iter() {
-			let filter = FilterBuf::new(filter)?;
-			filters.push((filter, qos))
+			filters.push((filter.try_into()?, qos))
 		}
 		Ok(Self(filters))
 	}
